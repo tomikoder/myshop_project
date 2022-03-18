02ba4453b564eebf0e2b1b55360c70340d5b5d2e
Tomek J <tommi_j@yahoo.com>
2022-03-18 18:58:06 +0100

    Dodałem parę rzeczy.

--- myshoop/myshoop/wsgi.py
+++ myshoop/myshoop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshoop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshoop.myshoop.settings')
 
 application = get_wsgi_application()
