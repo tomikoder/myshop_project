716f50c8f75d83f7230c9bdeff813eefaac915c9
Tomek J <tommi_j@yahoo.com>
2022-05-01 23:36:26 +0100

    Parę zmian

--- myshoop/static/js/mainscript.js
+++ myshoop/static/js/mainscript.js
@@ -145,7 +145,6 @@ $(function() {
 	});
 
 	$('#signupform').on('submit', function(event) {
-	    alert("A");
 	    event.preventDefault();
 	    ai = $('#signupform').serialize();
 	    mycookie = Cookies.get("csrftoken");
